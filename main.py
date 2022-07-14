a2a3a72500779d1102e824d4cffa806d97c1f7a8
BENJAMINAZOCAR <89295577+BENJAMINAZOCAR@users.noreply.github.com>
2022-07-14 10:32:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+from discord.ext import commands
+from discord.ext.commands.core import guild_only
+from discord.message import DeletedReferencedMessage
+
+bot = commands.Bot(command_prefix="##", case_insensitive=True, name="ChatNote")
+
+
+        ######ids adms####
+
+
+bot = commands.Bot(command_prefix="##", case_insensitive=True, name="ChatNote")
+
+class ChatNoteCommands(commands.Cog, name="ChatNote"):
+    @commands.command(
+        help="Adds a new note to your current notebook, or the specified notebook",
+        brief="Adds a note to a notebook",
+        pass_context=True
+    )
+    async def note(self, ctx, *, text):
+        note_text = text.strip()
+        await ctx.channel.send("Noted: " + note_text)
+
+
+
+
+    ################################333333333
+
+@bot.event
+async def on_message(message):
+
+
+        ######ids adms####
+    if message.author.id == 997129721550753862: #este id bot
+        return
+    ###if message.author.id == 591790331947778086: #mi id
+       ### return
+#######################################################config
+    user = message.author
+    user_id = message.author.id
+    channel = message.channel.id
+    content = message.content
+    channel = bot.get_channel(997127090568712282)###va
+    if message.content:
+     await channel.send(f"{content}")
+     await bot.process_commands(message)
+     return
+
+
+
+################################
+
+bot.run('OTk3MTI5NzIxNTUwNzUzODYy.GM3301.ME0u6fWeuKp8Q_VmgLHQgXAL1OnBzi_6d1zfwI')
